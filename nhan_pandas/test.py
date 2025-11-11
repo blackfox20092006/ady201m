d050932911880a946df0b7d8a01d63a521044d00
blackfox20092006 <xxhoangquangnhanxx@gmail.com>
2025-11-11 18:06:00 +0700

    Refactor display options for pandas DataFrame in test script

--- nhan_pandas/test.py
+++ nhan_pandas/test.py
@@ -1,4 +1,7 @@
 import pandas as pd
-pd.options.display.max_rows = 9999
+pd.options.display.max_rows = 9999      
+pd.options.display.max_columns = None  
+pd.options.display.width = 0           
+pd.options.display.max_colwidth = None
 df = pd.read_csv('vgsales.csv')
 print(df.head())
